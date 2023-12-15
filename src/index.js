cfd91d953f9a9fc48d3d297b98b718ae233d41fd
Neoxaslt <95044018+Neoxaslt@users.noreply.github.com>
2023-12-15 18:58:29 +0000

    formated date and descriptions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { Client, GatewayIntentBits, EmbedBuilder } from "discord.js";
 import DataOperations from "./DataOperations.js";
-import APIScheduler from './APIScheduler.js';
+import APIScheduler from "./APIScheduler.js";
 
 const client = new Client({
   intents: [
@@ -26,10 +26,12 @@ client.on("interactionCreate", async (interaction) => {
       try {
         const events = await new DataOperations().monthEvents();
         if (events === null || events.length < 1) {
-        interaction.reply("Neradau jokių žaidimų šį mėnesį 😢 pabandyk: https://sratas.lt/renginiai");
-        return
+          interaction.reply(
+            "Neradau jokių žaidimų šį mėnesį 😢 pabandyk: https://sratas.lt/renginiai",
+          );
+          return;
         }
-        
+
         const sortByStartDate = (a, b) => {
           const dateA = new Date(a.start);
           const dateB = new Date(b.start);
@@ -45,20 +47,24 @@ client.on("interactionCreate", async (interaction) => {
             const embed = new EmbedBuilder()
               .setTitle(response[e].title)
               .setURL(response[e].url)
-              .setDescription(`Startas: ${response[e].start}, Pabaiga: ${response[e].end}`)
+              .setDescription(
+                `🟢 Startas: ${response[e].start}, 🔴 Pabaiga: ${response[e].end}`,
+              )
               .setThumbnail(response[e].organizatorImage)
-              .setColor('Random')
+              .setColor("Random")
               .addFields(
                 {
                   name: "Data",
-                  value: response[e].start,
+                  value: `${response[e].start.slice(0, 10)} ${dayOfWeek(
+                    response[e].start.slice(0, 10),
+                  )}`,
                   inline: true,
                 },
                 {
                   name: "žaidimo nuoroda",
                   value: response[e].url,
                   inline: true,
-                }
+                },
               );
 
             embeds.push(embed);
@@ -76,115 +82,141 @@ client.on("interactionCreate", async (interaction) => {
         console.error(error);
       }
       break;
-      case "week":
-        try {
-          const events = await new DataOperations().weekEvents();
-          if (events === null || events.length < 1) {
-          interaction.reply("Neradau jokių žaidimų šią savaitę 😢 pabandyk: https://sratas.lt/renginiai");
-          return
+    case "week":
+      try {
+        const events = await new DataOperations().weekEvents();
+        if (events === null || events.length < 1) {
+          interaction.reply(
+            "Neradau jokių žaidimų šią savaitę 😢 pabandyk: https://sratas.lt/renginiai",
+          );
+          return;
+        }
+
+        const sortByStartDate = (a, b) => {
+          const dateA = new Date(a.start);
+          const dateB = new Date(b.start);
+
+          return dateA - dateB;
+        };
+        const response = events.sort(sortByStartDate);
+
+        if (response !== null && response.length > 0) {
+          const embeds = [];
+          for (let e = 0; e < response.length; e++) {
+            const embed = new EmbedBuilder()
+              .setTitle(response[e].title)
+              .setURL(response[e].url)
+              .setDescription(
+                `🟢 Startas: ${response[e].start}, 🔴 Pabaiga: ${response[e].end}`,
+              )
+              .setThumbnail(response[e].organizatorImage)
+              .setColor("Random")
+              .addFields(
+                {
+                  name: "Data",
+                  value: `${response[e].start.slice(0, 10)} ${dayOfWeek(
+                    response[e].start.slice(0, 10),
+                  )}`,
+                  inline: true,
+                },
+                {
+                  name: "žaidimo nuoroda",
+                  value: response[e].url,
+                  inline: true,
+                },
+              );
+
+            embeds.push(embed);
           }
 
-          const sortByStartDate = (a, b) => {
-            const dateA = new Date(a.start);
-            const dateB = new Date(b.start);
+          if (embeds.length > 0) {
+            console.log(embeds);
+            await interaction.reply({ embeds });
+          } else {
+            interaction.reply("Can't do that right now");
+            console.log("Received null or empty response");
+          }
+        }
+      } catch (error) {
+        console.error(error);
+      }
+      break;
+    case "big":
+      try {
+        const events = await new DataOperations().bigEvents();
+        if (events === null || events.length < 1) {
+          interaction.reply(
+            "Neradau jokių didelių žaidimų šiais metais 😢 pabandyk: https://sratas.lt/renginiai",
+          );
+          return;
+        }
+        const sortByStartDate = (a, b) => {
+          const dateA = new Date(a.start);
+          const dateB = new Date(b.start);
 
-            return dateA - dateB;
-          };
+          return dateA - dateB;
+        };
+
+        if (response !== null && response.length > 0) {
           const response = events.sort(sortByStartDate);
+          const embeds = [];
 
-          if (response !== null && response.length > 0) {
-            const embeds = [];
+          for (let e = 0; e < response.length; e++) {
+            const embed = new EmbedBuilder()
+              .setTitle(response[e].title)
+              .setURL(response[e].url)
+              .setDescription(
+                `🟢 Startas: ${response[e].start}, 🔴 Pabaiga: ${response[e].end}`,
+              )
+              .setThumbnail(response[e].organizatorImage)
+              .setColor("Random")
+              .addFields(
+                {
+                  name: "Data",
+                  value: `${response[e].start.slice(0, 10)} ${dayOfWeek(
+                    response[e].start.slice(0, 10),
+                  )}`,
+                  inline: true,
+                },
+                {
+                  name: "žaidimo nuoroda",
+                  value: response[e].url,
+                  inline: true,
+                },
+              );
 
-            for (let e = 0; e < response.length; e++) {
-              const embed = new EmbedBuilder()
-                .setTitle(response[e].title)
-                .setURL(response[e].url)
-                .setDescription(`Startas: ${response[e].start}, Pabaiga: ${response[e].end}`)
-                .setThumbnail(response[e].organizatorImage)
-                .setColor('Random')
-                .addFields(
-                  {
-                    name: "Data",
-                    value: response[e].start,
-                    inline: true,
-                  },
-                  {
-                    name: "žaidimo nuoroda",
-                    value: response[e].url,
-                    inline: true,
-                  }
-                );
-
-              embeds.push(embed);
-            }
-
-            if (embeds.length > 0) {
-              console.log(embeds);
-              await interaction.reply({ embeds });
-            } else {
-              interaction.reply("Can't do that right now");
-              console.log("Received null or empty response");
-            }
+            embeds.push(embed);
           }
-        } catch (error) {
-          console.error(error);
+
+          if (embeds.length > 0) {
+            console.log(embeds);
+            await interaction.reply({ embeds });
+          } else {
+            interaction.reply("Can't do that right now");
+            console.log("Received null or empty response");
+          }
         }
-        break;
-      case "big":
-        try {
-          const events = await new DataOperations().bigEvents();
-          if (events === null || events.length < 1) {
-            interaction.reply("Neradau jokių didelių žaidimų šiais metais 😢 pabandyk: https://sratas.lt/renginiai");
-            return
-          }
-          const sortByStartDate = (a, b) => {
-            const dateA = new Date(a.start);
-            const dateB = new Date(b.start);
-
-            return dateA - dateB;
-          };
-
-          if (response !== null && response.length > 0) {
-            const response = events.sort(sortByStartDate);
-            const embeds = [];
-
-            for (let e = 0; e < response.length; e++) {
-              const embed = new EmbedBuilder()
-                .setTitle(response[e].title)
-                .setURL(response[e].url)
-                .setDescription(`Startas: ${response[e].start}, Pabaiga: ${response[e].end}`)
-                .setThumbnail(response[e].organizatorImage)
-                .setColor('Random')
-                .addFields(
-                  {
-                    name: "Data",
-                    value: response[e].start,
-                    inline: true,
-                  },
-                  {
-                    name: "žaidimo nuoroda",
-                    value: response[e].url,
-                    inline: true,
-                  }
-                );
-
-              embeds.push(embed);
-            }
-
-            if (embeds.length > 0) {
-              console.log(embeds);
-              await interaction.reply({ embeds });
-            } else {
-              interaction.reply("Can't do that right now");
-              console.log("Received null or empty response");
-            }
-          }
-        } catch (error) {
-          console.error(error);
-        }
-        break;
+      } catch (error) {
+        console.error(error);
+      }
+      break;
   }
 });
+
+function dayOfWeek(date) {
+  const dateObj = new Date(date);
+  const weekDays = [
+    "Sekmadienis",
+    "Pirmadienis",
+    "Antradienis",
+    "Trečiadienis",
+    "Ketvirtadienis",
+    "Penktadienis",
+    "Šeštadienis",
+  ];
+  const weekDayIndex = dateObj.getUTCDay();
+  return weekDays[weekDayIndex];
+}
 
 
 
